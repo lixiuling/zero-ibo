4565f6d6fc885fc79a0bd33ccd85930586151c6a
lixiuling <xiulingli@shein.com>
2022-02-10 16:36:38 +0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import ReactDOM from 'react-dom'
+import './common/scss/common.scss'
+import { HomeApp } from './App'
+
+ReactDOM.render (
+  <HomeApp />,
   document.getElementById('root')
 )
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './index.scss'
+
+interface HomePropsType {
+  value: number | string
+}
+
+class HomeItem extends React.Component<HomePropsType> {
+  render() {
+    return (
+      <li className="homeList__item">
+        <span className="ibo-icon ibo-ziliaoku homeList__item-icon"></span>
+        <span> { this.props.value } </span>
+      </li>
+    )
+  }
+}
+
+/**
+ * ！！！if 语句以及 for 循环不是 JavaScript 表达式，所以不能在 JSX 中直接使用。但是，你可以用在 JSX 以外的代码中
+ * 使用时需要封装在函数里
+ */
+class HomeApp extends React.Component {
+  render() {
+    const homeTitle = 'welcome to ibo page!'
+    const listItems = () => {
+      let items = []
+      const curYear = new Date().getFullYear()
+      // 2015 年啵啵出道
+      for (let i = curYear; i > 2014; i-- ) {
+        const tempKey = `${i}year`
+        const temp = <HomeItem value={i} key={tempKey} />
+        items.push(temp)
+      }
+      return items
+    }
+
+    return (
+      <div className="iboHome">
+        <div className="iboHome__title"> { homeTitle } </div>
+        <ul className="iboHome__list">
+          { listItems() }
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default HomeApp
